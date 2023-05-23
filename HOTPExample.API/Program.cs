f36e4298213a7ad7885c89d9780e37632a671aba
lozturk <leventufukozturk@gmail.com>
2023-05-23 13:33:14 +0300

    memory cache

--- HOTPExample.API/Program.cs
+++ HOTPExample.API/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IHOTPGeneratorService, HOTPGeneratorService>();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
